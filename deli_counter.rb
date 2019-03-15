40b82201aba45beed29094466a47c45523a658a5
Justin Wu <justinhwu95@gmail.com>
2019-03-15 23:55:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
   else
     i=0
     for i in i..katz_deli.length do
-      katz_deli.push("The line is currently: #{i+1}. #{katz_deli[i]}")
+      return "The line is currently: #{i+1}. #{katz_deli[i]}"
     end
   end
 end
