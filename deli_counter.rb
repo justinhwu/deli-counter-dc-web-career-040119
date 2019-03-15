6fd6407944e44cbf26823ec40ce5dcd5ea655fd4
Justin Wu <justinhwu95@gmail.com>
2019-03-15 23:59:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,4 @@
 # Write your code here.
 def line(katz_deli)
   
-  line = []
-  if katz_deli.length ==0 
-    puts "The line is currently empty."
-  else
-    i=0
-    for i in i..katz_deli.length do
-      return "#{i+1}. #{katz_deli[i]}"
-    end
-  end
 end
