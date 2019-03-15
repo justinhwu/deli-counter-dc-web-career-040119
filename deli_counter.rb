08bdb4607b00dba835727fefe1bdf1d7e76b8f0a
Justin Wu <justinhwu95@gmail.com>
2019-03-15 23:54:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     i=0
-    for i in i.katz_deli.length do
+    for i in i..katz_deli.length do
       puts "The line is currently: #{i+1}. #{katz_deli[i]}"
     end
   end
