de7cdcc9919f5d3b413762ad5a57552b6611f023
Justin Wu <justinhwu95@gmail.com>
2019-03-16 00:35:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ end
 
 def take_a_number(katz_deli, name)
   if katz_deli.length ==0
-    katz_deli.push("Welcome, #{name}. You are number 1 in line.")
+    katz_deli.push(name)
   else 
     katz.deli.push(name)
   end
